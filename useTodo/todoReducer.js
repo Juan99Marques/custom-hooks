aa8a6e80fb3dabe33e6dc6d26680c8f98a1f8b58
Juan99Marques <juanmarques@kadabrait.net>
2024-02-07 12:38:18 -0300

    use todos agregado

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,19 @@
+export const todoReducer = (initialState, action) => {
+
+    
+
+    switch(action.type) {
+        case '[TODO] add todo':
+            return [ ...initialState, action.payload ]
+        case '[TODO] delete todo':
+            return initialState.filter(todo => todo.id !== action.payload);
+        case '[TODO] toggle todo':
+            return initialState.map(todo => 
+                (todo.id === action.payload)
+                ? {...todo, done: !todo.done}
+                : todo
+            );
+        default:
+            return initialState;
+    }
+}
